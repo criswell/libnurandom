b74fef0cb4a0197dfd7e90319eaea1c7f1bc5320
Sam Hart <hartsn@gmail.com>
2011-08-24 13:12:55 -0400

    begin to flesh out header

--- src/nurandom.h
+++ src/nurandom.h
@@ -15,19 +15,72 @@
 */
 
 /*
- * Threaded random number library.
+ * Random number library.
  */
 
-/*! \mainpage nurandom, threaded random number library
+/*! \mainpage nurandom, random number library
  * \author Sam Hart
  * \copyright Apache License, Version 2.0
  * \version 2011.08.0
  *
- * nurandom is a threaded random number library that pulls random numbers from
- * a variety of external sources.
+ * nurandom is a random number library that pulls random numbers from  a
+ * variety of external sources.
  */
 
 #ifndef nurandom_H
 #define nurandom_H
+
+//! The size of the memory pool of random ints (in 64bit Integers)
+#define MEMORY_POOL_SIZE 256
+
+//! The struct for the external sources toggles
+struct NuRandom_ExternalSources
+{
+    //! Use /dev/urandom (T/F)
+    bool Urandom;
+
+    //! Use random.org
+    bool RandomOrg
+};
+
+class NuRandom
+{
+    private:
+        int64_t numPool[MEMORY_POOL_SIZE];
+
+        int currentCount;
+
+    public:
+        //! Default constructor for nurandom class
+        NuRandom();
+
+        //! Constructor for nurandom class
+        /*!
+         * Use this constructor to determine which external sources to use.
+         *
+         * \param sources Structure definining which external sources to use.
+         *
+         * \remarks At least one of the structures for external sources must
+         * be enabled. If none of them are, the /dev/urandom one will be
+         * automatically enabled.
+         */
+        NuRandom(NuRandom_ExternalSources sources);
+
+        //! Destructor for nurandom class
+        ~NuRandom();
+
+        //! Get the next random number
+        /*!
+         * Pulls the next random number from the pool. If no more random
+         * numbers exist, will populate the pool with more random numbers.
+         *
+         * \remarks Due to dependencies on external sources for random
+         * numbers, the pool poputlation might take a bit of time. You can
+         * tweak the external sources we use for random numbers (thus reducing
+         * potential delays, but possibly losing entropy) in the constructor
+         * or
+         */
+        int64_t nextRandom();
+};
 
 #endif /* nurandom_H */
