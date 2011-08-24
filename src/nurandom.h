794ed12404f737d533f53d3fbd38f5bf04fabd2c
Sam Hart <hartsn@gmail.com>
2011-08-24 12:52:09 -0400

    initial stubs

---
+++ src/nurandom.h
@@ -0,0 +1,33 @@
+/*
+   Copyright 2011 Sam Hart <hartsn@gmail.com>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+/*
+ * Threaded random number library.
+ */
+
+/*! \mainpage nurandom, threaded random number library
+ * \author Sam Hart
+ * \copyright Apache License, Version 2.0
+ * \version 2011.08.0
+ *
+ * nurandom is a threaded random number library that pulls random numbers from
+ * a variety of external sources.
+ */
+
+#ifndef nurandom_H
+#define nurandom_H
+
+#endif /* nurandom_H */
